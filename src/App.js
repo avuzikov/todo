319b2b4196d1941873049ff2f9b5f39477c6dc80
Alexander Uzikov <avuzikov@gmail.com>
2021-11-26 23:33:03 +0100

    Added lazy loading to App.js, added LoadingSpinner

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import React, { Suspense } from "react";
+
+/*import ToDoListPage from "./components/pages/ToDoListPage";
 import ChangePasswordPage from "./components/pages/ChangePasswordPage";
-import ToDoListPage from "./components/pages/ToDoListPage";
-import Layout from "./components/layout/Layout";
 import LoginPage from "./components/pages/LoginPage";
+import CreateProfilePage from "./components/pages/CreateProfilePage";
+*/
+import { Route, Routes, Navigate } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import { useSelector, useDispatch } from "react-redux";
 import { authActions } from "./store/auth";
-import CreateProfilePage from "./components/pages/CreateProfilePage";
 import Button from "./components/UI/Button";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const LoginPage = React.lazy(() => import("./components/pages/LoginPage"));
+const ChangePasswordPage = React.lazy(() =>
+  import("./components/pages/ChangePasswordPage")
+);
+const ToDoListPage = React.lazy(() =>
+  import("./components/pages/ToDoListPage")
+);
+const CreateProfilePage = React.lazy(() =>
+  import("./components/pages/CreateProfilePage")
+);
+
+/* */
 
 function App() {
   const dispatch = useDispatch();
@@ -22,22 +38,30 @@ function App() {
   });
   return (
     <React.Fragment>
-      {!loggedIn && (
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/new" element={<CreateProfilePage />} />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      )}
-      {loggedIn && (
-        <Layout>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        {!loggedIn && (
           <Routes>
-            <Route path="/" element={<ToDoListPage />} />
-            <Route path="/change-password" element={<ChangePasswordPage />} />
-            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/new" element={<CreateProfilePage />} />
+            <Route path="*" element={<Navigate to="/login" />} />
           </Routes>
-        </Layout>
-      )}
+        )}
+        {loggedIn && (
+          <Layout>
+            <Routes>
+              <Route path="/" element={<ToDoListPage />} />
+              <Route path="/change-password" element={<ChangePasswordPage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </Layout>
+        )}
+      </Suspense>
       <Button onClick={loggedIn ? logoutHandler : loginHandler}>
         Change Login
       </Button>
